8b9db0a264801246761b6c2797e93d2b813419ef
cmendez <christian.velez55@gmail.com>
2025-07-14 01:59:58 -0400

    Added small rock spawning.

--- components/rock/rock.gd
+++ components/rock/rock.gd
@@ -5,6 +5,8 @@ var size
 var radius
 var scale_factor = 0.2
 
+signal exploded
+
 func start(_position, _velocity, _size):
 	screensize = DisplayServer.screen_get_size()
 	position = _position
@@ -12,8 +14,8 @@ func start(_position, _velocity, _size):
 	mass = 1.5 * size
 	
 	# set explosion force based on size 
-	$Explosion.scale =  	Vector2.ONE * 0.75 * size
-		
+	$Explosion.scale = Vector2.ONE * 0.75 * size
+	
 	# we set the scale based on size given, and create the shape
 	$Sprite2D.scale = Vector2.ONE * scale_factor * size
 	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
@@ -31,4 +33,12 @@ func _integrate_forces(physics_state):
 	physics_state.transform = xform
 
 func explode():
-	pass
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
